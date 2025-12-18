b23dcff8f681de8854cd108e9795c917d8c0081b
Liubomyr Pryt <prytlubomyr@gmail.com>
2025-12-18 06:17:53 +0000

    Fixing redirection.

--- scripts/language-redirect.js
+++ scripts/language-redirect.js
@@ -1,4 +1,4 @@
-if (navigator.language === "uk")
-{
-    window.location.replace("/index.ua_uk.html");
+if (navigator.language === "uk") {
+    window.location.replace("index.ua_uk.html");
 }
+// added this comment because without it liveserver adds \ instead, and it breaks the script
